59ba1f7ab81f9ef08523b5cd48964bfcc856d08a
Abdurrahman Hafez <abdurrahman.hafez@gmail.com>
2023-12-21 10:21:52 +0200

    feat(dwmblocks): add dwmblocks to dwm

--- patched-dwm-6.4/config.h
+++ patched-dwm-6.4/config.h
@@ -2,7 +2,7 @@
 #include <X11/XF86keysym.h>
 /* appearance */
 static const unsigned int borderpx  	    =   3;							/* border pixel of windows */
-static const unsigned int gappx     	    =   10;							/* border pixel of windows */
+static const unsigned int gappx     	    =   0;							/* border pixel of windows */
 static const unsigned int snap      	    =   32;
 static const unsigned int systraypinning    =   0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft     =   0;    /* 0: systray in the right corner, >0: systray on left of status text */
@@ -32,7 +32,7 @@ static const char col_green[]       =   "#ABE9B3";
 static const char col_red[] 		=   "#F28FAD";
 static const char col_orange[]      =   "#F8BD96";
 static const char col_yellow[] 		=   "#FAE3B0";
-static const char col_pink[] 		=   "#d5aeea";
+static const char col_pink[] 		=   "#94E2D5"; // $d5aeea
 static const char col_blu[] 		=   "#008080";
 static const char col_borderbar[] 	=   "#1E1D2D"; // inner border
 
